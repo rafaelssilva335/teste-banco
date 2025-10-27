ba560aeed6cc50d04a93261a6156db2724143ac3
Rafael Silva <rafaelssilva35@gmail.com>
2025-10-26 21:10:12 -0300

    chore: initial domain structure setup

---
+++ app/Domain/Repositories/AccountRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Domain\Repositories;
+
+use App\Domain\Entities\Account;
+
+interface AccountRepository
+{
+    public function findById(string $id): ?Account;
+    public function save(Account $account): void;
+    public function clear(): void;
+}
